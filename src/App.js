31b00e62c032f0263846c341d186f7c95f2a2725
DESKTOP-D5NHP53\Maaz Javaid <mmaazj323@gmail.com>
2022-06-12 08:47:18 +0500

    Dies added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
+import Die from "./components/Die";
 import './style.css'
-function App() {
+export default function App() {
   return (
-    
-    <main className="main">
-      
-    </main>
-  );
+      <main>
+          <div className="dice-container">
+              <Die value="1" />
+              <Die value="2" />
+              <Die value="3" />
+              <Die value="4" />
+              <Die value="5" />
+              <Die value="6" />
+              <Die value="1" />
+              <Die value="1" />
+              <Die value="1" />
+              <Die value="1" />
+          </div>
+      </main>
+  )
 }
 
-export default App;
