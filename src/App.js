dd06fd92601f7827353ccedda44abe5250d8ca1c
DESKTOP-D5NHP53\Maaz Javaid <mmaazj323@gmail.com>
2022-06-13 08:38:06 +0500

    use effect added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Die from "./components/Die";
 import { nanoid } from "nanoid";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import "./style.css";
 //Feature updated
 export default function App() {
   const [Dies, setDies] = useState(allNewDice());
-
+  const [tenzies,settenzies]=useState(false);
+  useEffect(()=>{
+    console.log("Dies array changed")
+  },[Dies])
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
